3394b15b8ae90317fd2daa9d5a34235a5e37080c
Chakshu Tandon <chakshutandon@gmail.com>
2019-12-23 16:30:18 -0500

    Consider ppkey_mutex with TLB invalidation

--- mem_mutex_lat.c
+++ mem_mutex_lat.c
@@ -1,3 +1,5 @@
+#define URCU_INLINE_SMALL_FUNCTIONS 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -5,6 +7,9 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
+
+#include <urcu.h>
+#include <urcu/urcu-memb.h>
 
 #define MILLION (unsigned long long) 1e6
 #define BILLION (unsigned long long) 1e9
@@ -35,7 +40,7 @@ void *do_thread_run(void *_args) {
 
     timeit(
         pthread_mutex_lock(&args->lock);
-        *args->ppkey = 1;
+        *args->ppkey = 1 - *args->ppkey;
         pthread_mutex_unlock(&args->lock)
     , MILLION);
 
@@ -44,14 +49,14 @@ void *do_thread_run(void *_args) {
     pthread_exit(res);
 }
 
-int main() {    
+void test_pthread_mutex() {
     uint32_t ppkey;
 
     struct thread_args *args = malloc(sizeof(struct thread_args));
     args->ppkey = &ppkey;
     
     if (pthread_mutex_init(&args->lock, NULL) != 0) { 
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     long double avg_lat;
@@ -75,6 +80,60 @@ int main() {
 
         printf("Threads: %d, Average Latency: %Lf ns\n", nr_threads, avg_lat);
     }
+}
 
-    return EXIT_SUCCESS;
+// void *do_thread_run_rcu(void *_args) {
+//     struct thread_args *args = (struct thread_args *) _args;
+//     uint32_t *ppkey;
+
+//     urcu_memb_register_thread();
+
+//     timeit(
+//         urcu_memb_read_lock();
+//         ppkey = rcu_dereference(args->ppkey);
+//         rcu_assign_pointer(args->ppkey, ppkey);
+//         urcu_memb_read_unlock()
+//     , MILLION);
+
+//     // rcu_unregister_thread();
+// }
+
+// void test_rcu_prim() {
+//     uint32_t ppkey;
+
+//     struct thread_args *args = malloc(sizeof(struct thread_args));
+//     args->ppkey = &ppkey;
+
+//     int nr_threads;
+//     for (nr_threads = 1; nr_threads <= NR_THREADS_MAX; nr_threads *= 2) {
+//         pthread_t tid[nr_threads];
+//         long double *latency[nr_threads];
+//         int i;
+//         for (i = 0; i < nr_threads; i++) {
+//             pthread_create(&tid[i], NULL, do_thread_run_rcu, args); 
+//         }
+//         for (i = 0; i < nr_threads; i++) {
+//             pthread_join(tid[i], (void **)&latency[i]);
+//         }
+//         // avg_lat = 0;
+//         // for (i = 0; i < nr_threads; i++) {
+//         //     avg_lat += *latency[i];
+//         // }
+//         // avg_lat = avg_lat / nr_threads;
+
+//         // printf("Threads: %d, Average Latency: %Lf ns\n", nr_threads, avg_lat);
+//     }
+// }
+
+int main() {
+    // urcu_memb_init();
+    printf("-----------------------------------------------\n");
+    printf("pthread mutex:\n");
+    test_pthread_mutex();
+    printf("-----------------------------------------------\n");
+    printf("read-copy-update (RCU):\n");
+    // test_rcu_prim();
+    printf("-----------------------------------------------\n");
+
+    exit(EXIT_SUCCESS);
 }
